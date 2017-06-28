40bb1c776c9c89d3cb0888ba84a6ad92666a5827
azuo0730 <azuo0730@gmail.com>
2017-06-29 00:16:32 +0900

    テストデータコミット。

---
+++ pronamaMusicGame/Assets/Scripts/GameMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int a = 10;
+	}
+}
