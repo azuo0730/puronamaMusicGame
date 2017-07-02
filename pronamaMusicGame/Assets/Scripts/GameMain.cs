e7984d68ec0a98daed03b37da85e945e831fe771
azuo0730 <azuo0730@gmail.com>
2017-07-02 11:20:55 +0900

    ignore調整。 グリッドが流れるとこまで。

--- pronamaMusicGame/Assets/Scripts/GameMain.cs
+++ pronamaMusicGame/Assets/Scripts/GameMain.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameMain : MonoBehaviour {
 
+	private int musicBPM;					// 楽曲のBPM
+
+
 	// Use this for initialization
 	void Start ()
 	{
-
+		musicBPM = 120;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		int a = 10;
 	}
 }
---
+++ pronamaMusicGame/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	/// グリッド表示用オブジェクト
+	public Object GRID_OBJECT;
+	/// グリッドオブジェクトの表示数
+	public readonly int GRID_DRAW_NUM = 10;
+	/// グリッドのデフォルト表示座標
+	private readonly Vector3 GRID_DEFAULT_DRAW_POS = new Vector3(0.0f, 0.72f, 1.0f);
+
+	private List<GameObject> m_gridObject;
+
+	private float m_gridMoveTmp;
+
+	// Use this for initialization
+	void Start () {
+
+		m_gridMoveTmp = 0.0f;
+
+		float zOffset = 0;
+		float zOffsetAdd = -1.0f;
+		m_gridObject = new List<GameObject>();
+		m_gridObject.Clear();
+		for (int i=0; i<GRID_DRAW_NUM; i++)
+		{
+			GameObject tmp = Instantiate(GRID_OBJECT, GRID_DEFAULT_DRAW_POS + new Vector3(0, 0, zOffset), Quaternion.identity) as GameObject;
+			m_gridObject.Add( tmp );
+			zOffset += zOffsetAdd;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float moveAdd = 0.01f;
+		float depthMax = 1.0f;
+		m_gridMoveTmp += moveAdd;
+
+		for (int i=0; i<m_gridObject.Count; i++)
+		{
+			Transform trans = m_gridObject[i].transform;
+			Vector3 newPos = trans.position;
+			newPos.z += moveAdd;
+			if (m_gridMoveTmp >= depthMax)
+			{
+				newPos.z -= depthMax;
+			}
+			trans.SetPositionAndRotation(newPos, trans.rotation);
+		}
+		if (m_gridMoveTmp >= depthMax)
+		{
+			m_gridMoveTmp -= depthMax;
+		}
+	}
+}
+
+
