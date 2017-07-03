4adb84c122039c4be6a4fd61a945379506dbd51d
azuo0730 <azuo0730@gmail.com>
2017-07-03 23:29:47 +0900

    ノートが流れるところまで作成。

--- pronamaMusicGame/Assets/Scripts/MusicManager.cs
+++ pronamaMusicGame/Assets/Scripts/MusicManager.cs
@@ -9,24 +9,21 @@ public class MusicManager : MonoBehaviour {
 	/// グリッドオブジェクトの表示数
 	public readonly int GRID_DRAW_NUM = 10;
 	/// グリッドのデフォルト表示座標
-	private readonly Vector3 GRID_DEFAULT_DRAW_POS = new Vector3(0.0f, 0.72f, 1.0f);
+	private readonly Vector3 GRID_DEFAULT_DRAW_POS = new Vector3(0.0f, 0.72f, 0.5f);
 
-	private List<GameObject> m_gridObject;
-
-	private float m_gridMoveTmp;
+	private GameObject m_gridManager;
 
 	// Use this for initialization
 	void Start () {
-		m_gridMoveTmp = 0.0f;
-
 		float zOffset = 0;
 		float zOffsetAdd = -1.0f;
-		m_gridObject = new List<GameObject>();
-		m_gridObject.Clear();
+		m_gridManager = GameObject.Find("MusicScore/GridManager");
+		if(m_gridManager == null) return;
+
 		for (int i=0; i<GRID_DRAW_NUM; i++)
 		{
-			GameObject tmp = Instantiate(GRID_OBJECT, GRID_DEFAULT_DRAW_POS + new Vector3(0, 0, zOffset), Quaternion.identity) as GameObject;
-			m_gridObject.Add( tmp );
+			GameObject tmp = Instantiate(GRID_OBJECT, GRID_DEFAULT_DRAW_POS + new Vector3(0, 0, zOffset), Quaternion.identity, m_gridManager.transform) as GameObject;
+			if( tmp == null ) break;
 			zOffset += zOffsetAdd;
 		}
 	}
@@ -49,24 +46,16 @@ public class MusicManager : MonoBehaviour {
 	{
 		float moveAdd = 0.01f;
 		float depthMax = 1.0f;
-		m_gridMoveTmp += moveAdd;
 
 		// グリッドを流す処理
-		for (int i = 0; i < m_gridObject.Count; i++)
+		Transform trans = m_gridManager.transform;
+		Vector3 newPos = trans.position;
+		newPos.z += moveAdd;
+		if (newPos.z >= depthMax)
 		{
-			Transform trans = m_gridObject[i].transform;
-			Vector3 newPos = trans.position;
-			newPos.z += moveAdd;
-			if (m_gridMoveTmp >= depthMax)
-			{
-				newPos.z -= depthMax;
-			}
-			trans.SetPositionAndRotation(newPos, trans.rotation);
+			newPos.z -= depthMax;
 		}
-		if (m_gridMoveTmp >= depthMax)
-		{
-			m_gridMoveTmp -= depthMax;
-		}
+		trans.SetPositionAndRotation(newPos, trans.rotation);
 	}
 
 	/// <summary>
---
+++ pronamaMusicGame/Assets/Scripts/MusicScore.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 譜面１曲分全部を管理するクラス
+/// </summary>
+public class MusicScore : MonoBehaviour {
+
+	List<Note>			m_scoreNoteList;			// 譜面１曲分全部のノート
+	float				m_timeElapsed;				// 経過時間
+
+	GameObject			m_scoreManager;				// スコア管理オブジェクト (全てのスコアの親になる)
+
+	/// <summary>
+	/// 譜面１つを管理するクラス
+	/// </summary>
+	private class Note
+	{
+		private float			m_timing;
+		private Vector3			m_rotate;
+		private GameObject		m_obj;
+
+		public Note(float timing, Vector3 rotate)
+		{
+			m_timing = timing;
+			m_rotate = rotate;
+			m_obj = null;
+		}
+		~Note(){}
+
+		public float GetTiming(){ return m_timing; }
+		public GameObject GetObj(){ return m_obj; }
+		public void SetObj(GameObject obj){ m_obj = obj; }
+
+		public void SetObjPos(Vector3 pos)
+		{
+			if( m_obj == null ) return;
+			m_obj.transform.position = pos;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		m_scoreNoteList = new List<Note>();
+		m_scoreNoteList.Add(new Note(2.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(2.25f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(2.5f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(2.75f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(3.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+
+		m_scoreNoteList.Add(new Note(4.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(4.5f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(5.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(5.5f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(6.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(6.5f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(7.0f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(7.25f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_scoreNoteList.Add(new Note(7.5f, new Vector3(0.0f, 0.0f, 0.0f)));
+		m_timeElapsed = 0;
+
+		m_scoreManager = GameObject.Find("MusicScore/ScoreManager");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		m_timeElapsed += Time.deltaTime;
+
+		Debug.DebugText("TimeElapsed", m_timeElapsed.ToString("00000.00"), 0, 32);
+
+		float showTime = m_timeElapsed + 5.0f;
+		for(int i=0; i< m_scoreNoteList.Count; i++)
+		{
+			if(showTime >= m_scoreNoteList[i].GetTiming() )
+			{
+				if( m_scoreNoteList[i].GetObj() == null )
+				{
+					// 新規作成
+					Vector3 pos = new Vector3(0, 0, -m_scoreNoteList[i].GetTiming()) + m_scoreManager.transform.position;
+					m_scoreNoteList[i].SetObj( Instantiate(Resources.Load("Line/Prefab/Note"), pos, Quaternion.identity, m_scoreManager.transform) as GameObject );
+				}
+			}
+		}
+
+		float moveAdd = 0.01f;
+		m_scoreManager.transform.position += new Vector3(0, 0, moveAdd);
+	}
+}
